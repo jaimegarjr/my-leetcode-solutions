368a54bba38e679eda8ff3d080e4a549a139b1f0
Jaime Garcia, Jr <40704782+JJgar2725@users.noreply.github.com>
2022-05-24 10:43:11 -0500

    Time: 84 ms (27.67%), Space: 14.4 MB (97.38%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -4,4 +4,26 @@ class Solution:
         Runtime: O(nlogn)
         Space: O(n)
         """
-        return sorted(s) == sorted(t) if True else False
+        # return sorted(s) == sorted(t) if True else False
+        
+        """
+        Runtime: O(n)
+        Space: O(n) -> O(1), bcs 26 dis. chars
+        """
+        freq = {}
+        
+        for c in s:
+            if c in freq:
+                freq[c] += 1
+            else:
+                freq[c] = 1
+        
+        for c in t:
+            if c in freq:
+                freq[c] -= 1
+                if freq[c] == 0:
+                    del freq[c]
+            else:
+                return False
+        
+        return len(freq) == 0 if True else False
