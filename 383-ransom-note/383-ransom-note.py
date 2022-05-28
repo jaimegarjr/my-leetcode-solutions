8af982c4350843f335bee02b09af043b828b736b
Jaime Garcia, Jr <40704782+JJgar2725@users.noreply.github.com>
2022-05-28 14:04:16 -0500

    Time: 136 ms (14.78%), Space: 14.1 MB (94.36%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.py
@@ -0,0 +1,25 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        """
+        Runtime: O(n + m), where n is len(magazine) and m is len(ransomNote)
+        Space: O(n), where n is len(magazine)
+        """
+        freq = {}
+        
+        for c in magazine:
+            if c not in freq:
+                freq[c] = 1
+            else:
+                freq[c] += 1
+        
+        print(freq)
+        
+        for c in ransomNote:
+            if c in freq:
+                freq[c] -= 1
+                if freq[c] == 0:
+                    del freq[c]
+            else:
+                return False
+        
+        return True
