64e090b28e7683f40d1a61ec5dac968bb9f6b480
Jaime Garcia, Jr <40704782+JJgar2725@users.noreply.github.com>
2022-07-23 11:48:02 -0500

    Time: 76 ms (5.36%), Space: 14 MB (32.87%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.py
@@ -0,0 +1,28 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        """
+        Recursive Soln
+        if (m == 1 or n == 1):
+            return 1
+        
+        return self.uniquePaths(m-1, n) + self.uniquePaths(m, n-1)
+        """
+        
+        """
+        DP Soln
+        Runtime: O(n * m)
+        Space: O(n * m)
+        """
+        dp = [[0] * n for _ in range(m)]
+        
+        for i in range(n):
+            dp[m-1][i] = 1
+        for i in range(m):
+            dp[i][n-1] = 1
+        
+        for x in range(m - 2, -1, -1):
+            for y in range(n - 2, -1, -1):
+                if dp[x][y] == 0:
+                    dp[x][y] = dp[x + 1][y] + dp[x][y + 1]
+        
+        return dp[0][0]
