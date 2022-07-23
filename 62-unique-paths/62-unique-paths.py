ee126fc93cef86e5e6a8d6bf3f407d56dc0d8f46
Jaime Garcia, Jr <40704782+JJgar2725@users.noreply.github.com>
2022-07-23 11:55:18 -0500

    Time: 53 ms (38.61%), Space: 13.8 MB (73.29%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -13,15 +13,30 @@ class Solution:
         Runtime: O(n * m)
         Space: O(n * m)
         """
-        dp = [[0] * n for _ in range(m)]
+#         dp = [[0] * n for _ in range(m)]
         
-        for i in range(n):
-            dp[m-1][i] = 1
-        for i in range(m):
-            dp[i][n-1] = 1
+#         for i in range(n):
+#             dp[m-1][i] = 1
+#         for i in range(m):
+#             dp[i][n-1] = 1
         
-        for x in range(m - 2, -1, -1):
-            for y in range(n - 2, -1, -1):
-                dp[x][y] = dp[x + 1][y] + dp[x][y + 1]
+#         for x in range(m - 2, -1, -1):
+#             for y in range(n - 2, -1, -1):
+#                 dp[x][y] = dp[x + 1][y] + dp[x][y + 1]
         
-        return dp[0][0]
+#         return dp[0][0]
+        
+        """
+        DP Soln (NeetCode)
+        Runtime: O(n * m)
+        Space: O(n)
+        """
+        row = [1] * n
+        
+        for i in range(m - 1):
+            newRow = [1] * n
+            for j in range(n - 2, -1, -1):
+                newRow[j] = newRow[j + 1] + row[j]
+            row = newRow
+        
+        return row[0]
