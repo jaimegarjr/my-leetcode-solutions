31646fcb640a30183baa85c9d2d3c0408f8eb212
Jaime Garcia, Jr <40704782+jaimegarjr@users.noreply.github.com>
2022-08-17 10:21:19 -0500

    Time: 7206 ms (5.07%), Space: 26.4 MB (43.69%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.py
+++ 1861-rotating-the-box/1861-rotating-the-box.py
@@ -1,14 +1,17 @@
 class Solution:
     def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
+        # Runtime: O(m^2n)
+        # Space: O(mn)
         ROWS, COLS = len(box), len(box[0])
         rotatedBox = []
         
         # falling
-        for r in range(ROWS-1, -1, -1):
+        for r in range(ROWS-1, -1, -1): # O(nm^2)
             for c in range(COLS-1, -1, -1):
                 if box[r][c] == ".":
                     travColumn = c - 1
-                    while box[r][travColumn] == "." and travColumn >= 0: # shift
+                    # shift at most m times
+                    while box[r][travColumn] == "." and travColumn >= 0: 
                         travColumn -= 1
                     
                     if box[r][travColumn] == "*" or travColumn < 0:
@@ -19,7 +22,7 @@ class Solution:
                         box[r][travColumn] = "."
         
         # rotation
-        for i in range(COLS):
+        for i in range(COLS): # O(nm)
             newRow = []
             for j in range(ROWS-1, -1, -1):
                 newRow.append(box[j][i])
