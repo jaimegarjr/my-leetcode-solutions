e17ebc8de49911bec5d00a33003725a21824cdd4
Jaime Garcia, Jr <40704782+jaimegarjr@users.noreply.github.com>
2022-08-21 15:52:46 -0500

    Time: 446 ms (61.93%), Space: 21.9 MB (27.04%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,5 +1,7 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        # Runtime: O(v+e)
+        # Space: O(n)
         ROWS, COLS = len(grid), len(grid[0])
         count = 0
         visited = set()
