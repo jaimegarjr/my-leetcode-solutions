6af75f4fe66a78ce5ca9b21891c5fdfa92a5890f
Jaime Garcia, Jr <40704782+jaimegarjr@users.noreply.github.com>
2022-08-24 10:02:28 -0500

    Time: 6556 ms, Memory: 18.1 MB - LeetHub

---
+++ 3sum/3sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        triplets = []
+        
+        for i in range(len(nums)-2):
+            self.findTriplets(i, i + 1, len(nums)-1, triplets, nums)
+            
+        return triplets
+    
+    
+    def findTriplets(self, i, l, r, triplets, nums):
+        while l < r:
+            triplet = [nums[i], nums[l], nums[r]]
+            curSum = sum(triplet)
+            
+            if curSum == 0 and triplet not in triplets:
+                triplets.append(triplet)
+                l += 1
+            elif curSum < 0:
+                l += 1
+            else:
+                r -= 1
+        return
