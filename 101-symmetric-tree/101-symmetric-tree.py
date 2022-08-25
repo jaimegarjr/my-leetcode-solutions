796c77d2680a23848640dd3eaf1c36a4ba28a6b9
Jaime Garcia, Jr <40704782+jaimegarjr@users.noreply.github.com>
2022-08-25 11:45:57 -0500

    Time: 59 ms (36.95%), Space: 13.8 MB (99.66%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.py
+++ 101-symmetric-tree/101-symmetric-tree.py
@@ -6,8 +6,11 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        """
+        # DFS
         # Runtime: O(n)
         # Space: O(h or n)
+        
         def isSameTree(p, q):
             if not p and not q: return True
             if (not p and q) or (not q and p) or (p.val != q.val): return False
@@ -15,5 +18,23 @@ class Solution:
             return isSameTree(p.right, q.left) and isSameTree(p.left, q.right)
         
         return isSameTree(root.left, root.right)
-    
+        """
+        # BFS
+        # Runtime: O(n)
+        # Space: O(n)
+        q = collections.deque([root.left, root.right])
+        
+        while q:
+            t1 = q.popleft()
+            t2 = q.popleft()
+            
+            if not t1 and not t2: continue
+            if not t1 or not t2 or t1.val != t2.val: return False
+            q.append(t1.left)
+            q.append(t2.right)
+            q.append(t1.right)
+            q.append(t2.left)
+        
+        return True
+        
         
