d5337cc1b944e25e71115402708108ddbdf7a472
Jaime Garcia, Jr <40704782+jaimegarjr@users.noreply.github.com>
2022-08-26 11:32:41 -0500

    Time: 161 ms, Memory: 26 MB - LeetHub

--- lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -7,6 +7,8 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # Runtime: O(n)
+        # Space: O(n or h)
         def dfs(node):
             if not node:
                 return None
