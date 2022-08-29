6b655c6b33a9b9ea952e614b587028516356c258
Jaime Garcia, Jr <40704782+jaimegarjr@users.noreply.github.com>
2022-08-29 08:14:11 -0500

    Time: 556 ms (34.00%), Space: 22 MB (20.45%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,32 +1,26 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        # Runtime: O(v+e)
+        # Runtime: O(n)
         # Space: O(n)
         ROWS, COLS = len(grid), len(grid[0])
-        count = 0
         visited = set()
+        islands = 0
         
-        def dfs(r, c):
-            if (r < 0 or r >= ROWS or 
-                c < 0 or c >= COLS or 
-                (r, c) in visited or 
-                grid[r][c] == "0"):
+        def dfs(r,c):
+            if (r >= ROWS or c >= COLS or
+               r < 0 or c < 0 or
+               (r, c) in visited or grid[r][c] == "0"):
                 return
             
-            visited.add((r, c))            
-            directions = [
-                (-1, 0),
-                (0, 1),
-                (1, 0),
-                (0, -1)
-            ]
-            for x, y in directions:
-                dfs(r + x, c + y)
+            visited.add((r,c))
+            directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+            for dr, dc in directions:
+                dfs(r + dr, c + dc)
         
         for r in range(ROWS):
             for c in range(COLS):
                 if (r, c) not in visited and grid[r][c] == "1":
                     dfs(r, c)
-                    count += 1
+                    islands += 1
         
-        return count
+        return islands
