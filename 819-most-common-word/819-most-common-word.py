16178c239a88106a5b751da7454b57b605e70f32
Jaime Garcia, Jr <40704782+jaimegarjr@users.noreply.github.com>
2022-09-06 12:33:56 -0500

    Time: 46 ms (76.20%), Space: 14 MB (37.10%) - LeetHub

---
+++ 819-most-common-word/819-most-common-word.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        filtered = ""
+        for c in paragraph:
+            if c.isalnum():
+                filtered += c.lower()
+            else:
+                filtered += ' '
+
+        freqMap = collections.Counter(filtered.split())
+
+        for key, val in freqMap.most_common(len(freqMap)):
+            if key in banned:
+                continue
+            else:
+                return key
