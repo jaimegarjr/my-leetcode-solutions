2c96ae4d3a61d83fe7c2363ec3904b206cf2b67f
Jaime Garcia, Jr <40704782+jaimegarjr@users.noreply.github.com>
2022-09-08 11:31:54 -0500

    Time: 3308 ms (21.46%), Space: 13.9 MB (90.20%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -2,25 +2,20 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         maxSub = ""
 
-        # uneven string length pass
         for i in range(len(s)):
+            # uneven string length pass
             l = r = i
-            while l >= 0 and r <= len(s)-1:
+            while l >= 0 and r <= len(s)-1 and s[l] == s[r]:
                 subStr = s[l:r+1]
-                if s[l] != s[r]:
-                    break
                 if len(subStr) > len(maxSub):
                     maxSub = subStr
                 l -= 1
                 r += 1
 
-        # even string length pass
-        for i in range(len(s)):
+            # even length pass
             l, r = i, i + 1
-            while l >= 0 and r <= len(s)-1:
+            while l >= 0 and r <= len(s)-1 and s[l] == s[r]:
                 subStr = s[l:r+1]
-                if s[l] != s[r]:
-                    break
                 if len(subStr) > len(maxSub):
                     maxSub = subStr
                 l -= 1
