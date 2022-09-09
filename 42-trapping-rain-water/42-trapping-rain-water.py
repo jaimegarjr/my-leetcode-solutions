e0910af82ae45825116b8d43c65462f97b4b4cfd
Jaime Garcia, Jr <40704782+jaimegarjr@users.noreply.github.com>
2022-09-09 10:29:16 -0500

    Time: 389 ms (5.00%), Space: 16 MB (81.43%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -6,19 +6,17 @@ class Solution:
         l, r = 0, len(height)-1
         leftMax, rightMax = 0, 0
         
-        while l <= r:
-            # update maxes
-            leftMax = max(leftMax, height[l])
-            rightMax = max(rightMax, height[r])
-            
+        while l < r:
             # determine if dip, update res if so
             if height[l] < leftMax:
                 res += leftMax - height[l]
             
-            if l == r: break
-            
             if height[r] < rightMax:
                 res += rightMax - height[r]
+            
+            # update maxes
+            leftMax = max(leftMax, height[l])
+            rightMax = max(rightMax, height[r])
             
             # update pointers
             if leftMax <= rightMax: 
