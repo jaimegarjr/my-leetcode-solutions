29668e1393c96de8e737aa46d43a1ef3db8ec1fd
Jaime Garcia, Jr <40704782+jaimegarjr@users.noreply.github.com>
2022-09-09 09:14:44 -0500

    Time: 161 ms (77.07%), Space: 16 MB (44.94%) - LeetHub

---
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -0,0 +1,32 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        res = 0
+        l, r = 0, len(height)-1
+        leftMax, rightMax = 0, 0
+        
+        while l <= r:
+            # update maxes
+            leftMax = max(leftMax, height[l])
+            rightMax = max(rightMax, height[r])
+            
+            # determine if dip, update res if so
+            if height[l] < leftMax or height[l] < rightMax:
+                side = min(leftMax, rightMax)
+                res += side - height[l]
+            
+            if l == r: break
+            
+            if height[r] < rightMax or height[r] < leftMax:
+                side = min(leftMax, rightMax)
+                res += side - height[r]
+            
+            # update pointers
+            if leftMax < rightMax: 
+                l += 1
+            elif rightMax < leftMax: 
+                r -= 1
+            else: 
+                l += 1
+                r -= 1
+        
+        return res
