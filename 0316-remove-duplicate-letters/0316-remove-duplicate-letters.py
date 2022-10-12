2ef0e07aee98dab7811d1229ce40b4699532804a
Jaime Garcia, Jr <40704782+jaimegarjr@users.noreply.github.com>
2022-10-12 09:22:50 -0500

    Time: 32 ms (98.10%), Space: 14 MB (37.50%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
@@ -3,19 +3,18 @@ class Solution:
         stack = []
         visited = set()
         latestOccurence = {}
-        
+
         for i, c in enumerate(s):
-            latestOccurence[c] = i 
-        
-        
+            latestOccurence[c] = i
+
         for i, c in enumerate(s):
             if c not in visited:
                 while stack and stack[-1] > c and latestOccurence[stack[-1]] > i:
                     prevC = stack.pop()
                     visited.remove(prevC)
-                
+
                 stack.append(c)
                 visited.add(c)
-            
+
         return "".join(stack)
         
