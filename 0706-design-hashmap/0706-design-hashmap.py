d47c9460d035389281eb0a6957fbc8c01c6e91e5
Jaime Garcia, Jr <40704782+jaimegarjr@users.noreply.github.com>
2022-10-14 17:09:56 -0500

    Time: 272 ms (90.01%), Space: 17.5 MB (51.72%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.py
+++ 0706-design-hashmap/0706-design-hashmap.py
@@ -2,12 +2,10 @@ class ListNode:
     def __init__(self, key=None, value=None):
         self.key = key
         self.value = value
-        self.next = None
 
 
 class MyHashMap:
     def __init__(self):
-        self.size = 1000
         self.table = collections.defaultdict(ListNode)
 
     def put(self, key: int, value: int) -> None:
