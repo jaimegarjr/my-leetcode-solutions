5e7dc5b113a415f9fa6fae636641f4666b93175e
Jaime Garcia, Jr <40704782+jaimegarjr@users.noreply.github.com>
2022-10-15 09:30:57 -0500

    Time: 130 ms (98.62%), Space: 16.8 MB (49.37%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,5 +1,9 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        """
+        Runtime: O(n)
+        Space: O(1)
+        """
         n = nums[0]
         for num in nums[1:]:
             n ^= num
