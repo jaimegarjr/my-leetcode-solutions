8560a686182e811cece4f3ed45e1199a0771522c
Jaime Garcia, Jr <40704782+jaimegarjr@users.noreply.github.com>
2022-10-16 09:12:06 -0500

    Time: 653 ms (23.76%), Space: 15.9 MB (33.72%) - LeetHub

--- 0353-design-snake-game/0353-design-snake-game.py
+++ 0353-design-snake-game/0353-design-snake-game.py
@@ -29,16 +29,14 @@ class SnakeGame:
             dr + nr >= 0 and
             dc + nc < self.COLS and 
             dc + nc >= 0):
-            self.curSnakePos = [dr + nr, dc + nc]
-            self.snakeBodyPoints.appendleft(self.curSnakePos)
+            self.snakeBodyPoints.appendleft(newHead)
             
-            if (self.curSnakePos == self.curFoodPos):
+            if (newHead == self.curFoodPos):
                 self.snakeLen += 1
                 self.score += 1
                 self.curFoodPos = self.food[0] if self.food else [-1, -1]
                 self.food = self.food[1:]
-            
-            if self.snakeLen != len(self.snakeBodyPoints):
+            else:
                 self.snakeBodyPoints.pop()
             
             return self.score
