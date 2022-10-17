27b241dab9d706a9ed491d7bbbb91ddd83d81f06
Jaime Garcia, Jr <40704782+jaimegarjr@users.noreply.github.com>
2022-10-17 09:06:42 -0500

    Time: 82 ms (48.97%), Space: 13.9 MB (12.55%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,5 +1,8 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
+        """
+        Runtime: O(n)
+        Space: O(1)
         res, i = 0, 1
         while n > 0:
             n -= i
@@ -10,3 +13,18 @@ class Solution:
             res -= 1
         
         return res
+        """
+        left, right = 0, n
+        
+        while left <= right:
+            k = (left + right) // 2
+            cur = (k * (k + 1)) // 2
+            
+            if cur == n:
+                return k
+            if cur < n:
+                left = k + 1
+            else:
+                right = k - 1
+        
+        return right
