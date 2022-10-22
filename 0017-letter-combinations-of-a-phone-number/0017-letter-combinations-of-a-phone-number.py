84408ab30f026322da5aebf50f88a6bb9061405f
Jaime Garcia, Jr <40704782+jaimegarjr@users.noreply.github.com>
2022-10-22 17:22:47 -0500

    Time: 54 ms (55.45%), Space: 14.1 MB (31.54%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -2,24 +2,24 @@ class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if not digits:
             return []
-        numToLetter = {
-            "2": list("abc"),
-            "3": list("def"),
-            "4": list("ghi"),
-            "5": list("jkl"),
-            "6": list("mno"),
-            "7": list("pqrs"),
-            "8": list("tuv"),
-            "9": list("wxyz")
-        }
-        length = len(digits)
+
         res = []
-        
+        numToLetter = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"]
+        }
+
         def backtrack(digits, comb):
             if not digits:
                 res.append("".join(comb))
                 return
-            
+
             chars = numToLetter[digits[0]]
             for c in chars:
                 comb.append(c)
