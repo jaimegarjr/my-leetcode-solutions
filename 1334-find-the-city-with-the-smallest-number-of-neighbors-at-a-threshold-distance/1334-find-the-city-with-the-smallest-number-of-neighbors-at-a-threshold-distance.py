341ad53be23a243e5c04c985915e425c6e96daa8
Jaime Garcia, Jr <40704782+jaimegarjr@users.noreply.github.com>
2022-10-23 13:59:42 -0500

    Time: 1346 ms (51.85%), Space: 15.2 MB (45.27%) - LeetHub

---
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -0,0 +1,36 @@
+class Solution:
+    def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
+        graph = {i:[] for i in range(n)}
+        
+        for u, v, wt in edges:
+            graph[u].append((v, wt))
+            graph[v].append((u, wt))
+            
+        def dijkstras(city):
+            dist = [float('inf')] * n
+            dist[city] = 0
+            heap = []
+            heapq.heappush(heap, (0, city))
+            
+            while heap:
+                dis, node = heapq.heappop(heap)
+                
+                for adj in graph[node]:
+                    adj_c, adj_d = adj
+                    if adj_d + dis < dist[adj_c]:
+                        dist[adj_c] = adj_d + dis
+                        heapq.heappush(heap, (dist[adj_c], adj_c))
+            
+            return dist
+        
+        count = [0] * n
+        res = 0
+        for city in range(n):
+            distance = dijkstras(city)
+            for dis in distance:
+                if dis <= distanceThreshold:
+                    count[city] += 1
+            if count[city] <= count[res]:
+                res = city
+        
+        return res
