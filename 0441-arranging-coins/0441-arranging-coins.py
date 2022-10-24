8d13ef52ef978a3bf6a670b280602e0b63124123
Jaime Garcia, Jr <40704782+jaimegarjr@users.noreply.github.com>
2022-10-24 10:26:42 -0500

    Time: 712 ms (40.23%), Space: 13.8 MB (96.31%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,25 +1,13 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
         """
-        Exhaustive iterative soln
         Runtime: O(n)
         Space: O(1)
-        
-        Binary Search soln
-        Runtime: O(logn)
-        Space: O(1)
         """
-        left, right = 1, n
-        res = 0
-        
-        while left <= right:
-            mid = (left + right) // 2
-            coins = (mid * (mid + 1)) // 2
-            
-            if coins > n:
-                right = mid - 1
-            else:
-                res = max(mid, res)
-                left = mid + 1
-        
-        return res
+        coins = n
+        for i in range(1, n+1):
+            coins -= i
+            if coins == 0:
+                return i
+            elif coins < 0:
+                return i -1
