793ae670cb2add782ed2250c3f0a0e6c7492680d
Jaime Garcia, Jr <40704782+jaimegarjr@users.noreply.github.com>
2022-10-24 10:37:34 -0500

    Time: 41 ms (93.31%), Space: 13.8 MB (56.54%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,13 +1,20 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
         """
-        Runtime: O(n)
+        Runtime: O(logn)
         Space: O(1)
         """
-        coins = n
-        for i in range(1, n+1):
-            coins -= i
-            if coins == 0:
-                return i
-            elif coins < 0:
-                return i -1
+        l, r = 0, n
+        res = 0
+        
+        while l <= r:
+            m = (l + r) // 2
+            coins = (m * (m+1)) // 2
+            
+            if coins <= n:
+                res = max(m, res)
+                l = m + 1
+            else:
+                r = m - 1
+        
+        return res
