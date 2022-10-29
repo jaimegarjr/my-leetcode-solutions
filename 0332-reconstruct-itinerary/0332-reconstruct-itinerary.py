733b995ca3f9c439973cbaf8c948ce0e1f7d3083
Jaime Garcia, Jr <40704782+jaimegarjr@users.noreply.github.com>
2022-10-29 16:11:29 -0500

    Time: 236 ms (13.22%), Space: 14.9 MB (27.14%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -1,34 +1,40 @@
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
         """
-    Reconstructs an itinerary from a given tickets list
+        Reconstructs an itinerary from a given tickets list
         Runtime: O(e^2)
         Space: O(e)
         """
-        graph = collections.defaultdict(list)
-        tickets.sort()  # so in lexigraphical order
+        tickets.sort()  # sort in lexigraphical order
+        graph = {u: [] for u, v in tickets}
 
         # build graph
         for u, v in tickets:
             graph[u].append(v)
 
-        res = ["JFK"]
         def dfs(node):
+            # we have found a solution
             if len(res) == len(tickets) + 1:
                 return True
+
+            # no way to get back, backtrack
             if node not in graph:
                 return False
-            
+
+            # to avoid modifying list directly
             temp = list(graph[node])
             for i, nei in enumerate(temp):
                 res.append(nei)
                 graph[node].pop(i)
+
                 if dfs(nei):
                     return True
+
                 res.pop()
                 graph[node].insert(i, nei)
 
             return False
 
+        res = ["JFK"]
         dfs("JFK")
         return res
