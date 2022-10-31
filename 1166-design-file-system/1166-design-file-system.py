e2c9d3d0fc9d0249b317ca05af3cc2e0f3cc506e
Jaime Garcia, Jr <40704782+jaimegarjr@users.noreply.github.com>
2022-10-31 09:56:38 -0500

    Time: 264 ms (95.34%), Space: 21.7 MB (44.67%) - LeetHub

--- 1166-design-file-system/1166-design-file-system.py
+++ 1166-design-file-system/1166-design-file-system.py
@@ -1,25 +1,25 @@
 class FileSystem:
 
-	def __init__(self):
-		self.map = defaultdict(list)
+    def __init__(self):
+        self.paths = {}
 
-	def createPath(self, path: str, value: int) -> bool:
-		temp = path.split('/')
-		if len(temp)==2 and path not in self.map:
-			self.map[path] = value
-			return True
+    def createPath(self, path: str, value: int) -> bool:
+        if not path or path == '/' or path in self.paths:
+            return False
+        
+        parent = path[:path.rfind('/')]
+        if len(parent) > 1 and parent not in self.paths:
+            return False
+        
+        self.paths[path] = value
+        return True
+            
+            
+    def get(self, path: str) -> int:
+        return self.paths.get(path, -1)
 
-		parent = '/'.join(temp[:-1])
-		if parent not in self.map:
-			return False
 
-		if path not in self.map:
-			self.map[path] = value
-			return True
-
-		return False
-
-	def get(self, path: str) -> int:
-		if path not in self.map:
-			return -1
-		return self.map[path]
+# Your FileSystem object will be instantiated and called as such:
+# obj = FileSystem()
+# param_1 = obj.createPath(path,value)
+# param_2 = obj.get(path)
