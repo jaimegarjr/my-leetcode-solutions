9ea5016b89c6c810237534ee865bc0857fedb03d
Jaime Garcia, Jr <40704782+jaimegarjr@users.noreply.github.com>
2022-10-31 09:45:18 -0500

    Time: 500 ms (66.41%), Space: 21.6 MB (82.15%) - LeetHub

---
+++ 1166-design-file-system/1166-design-file-system.py
@@ -0,0 +1,25 @@
+class FileSystem:
+
+	def __init__(self):
+		self.map = defaultdict(list)
+
+	def createPath(self, path: str, value: int) -> bool:
+		temp = path.split('/')
+		if len(temp)==2 and path not in self.map:
+			self.map[path] = value
+			return True
+
+		parent = '/'.join(temp[:-1])
+		if parent not in self.map:
+			return False
+
+		if path not in self.map:
+			self.map[path] = value
+			return True
+
+		return False
+
+	def get(self, path: str) -> int:
+		if path not in self.map:
+			return -1
+		return self.map[path]
